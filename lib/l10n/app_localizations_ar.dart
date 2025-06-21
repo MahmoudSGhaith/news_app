57a35961ecbb7519217521b543f2048f971795bb
mahmoud ghaith <mahmoudsobhy8960@gmail.com>
2025-06-21 09:20:22 +0200

    -i make drawer screen that has goto home text button and app theme to change theming from night to light and app language from english to arabic - it has all app categorise and if you press on view all tab it convert to news screen

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,64 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get good_morning => 'صباح الخير';
+
+  @override
+  String get good_after_noon => 'مساء الخير';
+
+  @override
+  String get good_night => 'ليله سعيده';
+
+  @override
+  String get here_is_some_news_for_you => 'إليك بعض الأخبار لك';
+
+  @override
+  String get search => 'بحث';
+
+  @override
+  String get news_app => 'تطبيق الأخبار';
+
+  @override
+  String get go_to_home => 'اذهب إلى الرئيسية';
+
+  @override
+  String get theme => 'السمة';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get dark => 'داكن';
+
+  @override
+  String get light => 'فاتح';
+
+  @override
+  String get english => 'الإنجليزية';
+
+  @override
+  String get arabic => 'العربية';
+
+  @override
+  String get something_went_wrong => 'شيء ما حدث خطأ';
+
+  @override
+  String get try_again => 'حاول مجدداُ';
+
+  @override
+  String get view_all => 'اظهار الكل';
+
+  @override
+  String get no_data_here => 'لايوجد بيانات ';
+}
